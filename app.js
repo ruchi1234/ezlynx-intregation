574fda16c8adf33576a5b7d344a26e39b6f5b295
ruchi1234 <info.webnexus@gmail.com>
2019-04-04 12:22:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,32 +3,40 @@ const fs = require('fs');
 const parser = new xml2js.Parser({ attrkey: "ATTR" });
 var request = require("request");
 var base64 = require('base-64');
+var jsonxml = require('json2xml');
+var format = require('xml-formatter');
 
-let rawData = fs.readFileSync("rawData.xml", "utf8");
-var encodedData = base64.encode(rawData);
 
-console.log(encodedData);
+let rawData = fs.readFileSync("rawData.json", "utf8");
+var data = jsonxml(JSON.parse(rawData)) ;
 
-let xml_authentication_header = fs.readFileSync("ezlynx-soap-authentication.xml", "utf8");
-let xml_soap_body_opens = fs.readFileSync("ezlynx-soap-body_open.xml", "utf8");
-let xml_soap_body_close = fs.readFileSync("ezlynx-soap-body_close.xml", "utf8");
 
+let xml_head = '<?xml version="1.0" encoding="utf-8"?> <EZHOME xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.ezlynx.com/XMLSchema/Home/V200">' ;
+let xml_body = xml_head.concat(data, "</EZHOME>")
+
+var encodedData = base64.encode(xml_body);
+console.log(format(xml_body));
+
+
+let xml_authentication_header = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope  xmlns:soap="http://www.w3.org/2003/05/soap-envelope"  xmlns:tem="http://tempuri.org/"  xmlns:v100="http://www.ezlynx.com/XMLSchema/EZLynxUpload/V100">  <soap:Header>   <tem:AuthenticationHeaderAcct> <tem:Username>xi_uploadUAT</tem:Username>  <tem:Password>Cojoanin93</tem:Password>  <tem:AccountUsername>xi_userUAT</tem:AccountUsername>  </tem:AuthenticationHeaderAcct> </soap:Header>';
+let xml_soap_body_opens = '<soap:Body> <tem:UploadFile> <v100:EZLynxUploadRequest>  <v100:UploadRequest RefID="test123" XrefKey="DHO6" DataUploadFlags="4"><v100:FileData Name="EZHome" MimeType="text/xml">';
+let xml_soap_body_close = "</v100:FileData> </v100:UploadRequest> </v100:EZLynxUploadRequest> </tem:UploadFile> </soap:Body></soap:Envelope>";
 let xml_string = xml_authentication_header.concat(xml_soap_body_opens, encodedData, xml_soap_body_close);
 
-console.log(xml_string);
+
+console.log(format(xml_body));
 
 var options = { method: 'POST',
   url: 'https://uat.webcetera.com/EzLynxWebService/EzLynxFileUpload.asmx',
   qs: { WSDL: '' },
   headers: 
    { 
-     SOAPAction: 'http://tempuri.org/UploadFile',
-    
+     SOAPAction: 'http://tempuri.org/UploadFile',    
      'Content-Type': 'text/xml' },
   body: xml_string };
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
 
-  console.log(body);
+  console.log(format(body));
 }); 
